87d7e99def88518c711e05cdf933b41e59197bb6
Victoria Olumide <olumideronke@gmail.com>
2025-05-10 14:56:43 +0100

    uncommenting

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "../utils/config";
 
 const axiosInstance = axios.create({
-    baseURL: BASE_URL, // Replace with your actual API base URL
+    baseURL: BASE_URL,
     // withCredentials: true, // Ensures cookies are sent with requests
     headers: {
       "Content-Type": "application/json",
