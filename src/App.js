46c657c0b4b22c9c1b671561659514ecafbdfc09
Victoria Olumide <olumideronke@gmail.com>
2025-05-13 22:48:00 +0100

    Mobile2

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import Layout from './components/Layout/Layout';
 import './App.css';
 
 const App = () => {
-  // const dispatch = useDispatch();
-  // const navigate = useNavigate();
-  // const { pathname } = useLocation();
-  // const [initialized, setInitialized] = useState(false); // To prevent premature redirect
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const [initialized, setInitialized] = useState(false); // To prevent premature redirect
 
-  // const userData = localStorage.getItem('user');
+  const userData = localStorage.getItem('user');
 
-  // const isLoginPage =
-  //   pathname === '/login' ||
-  //   pathname === '/home' ||
-  //   pathname === '/' ||
-  //   pathname === '/get-started' ||
-  //   pathname === '/get-started/register' ||
-  //   pathname === '/verify';
+  const isLoginPage =
+    pathname === '/login' ||
+    pathname === '/home' ||
+    pathname === '/' ||
+    pathname === '/get-started' ||
+    pathname === '/get-started/register' ||
+    pathname === '/verify';
 
   // useEffect(() => {
   //   const storedUser = localStorage.getItem('user');
@@ -28,11 +28,11 @@ const App = () => {
   //   setInitialized(true);
   // }, [dispatch]);
 
-  // useEffect(() => {
-  //   if (initialized && !userData && !isLoginPage) {
-  //     navigate('/login');
-  //   }
-  // }, [initialized, userData, isLoginPage, navigate]);
+  useEffect(() => {
+    if (initialized && !userData && !isLoginPage) {
+      navigate('/login');
+    }
+  }, [initialized, userData, isLoginPage, navigate]);
 
   return <Layout />;
 };
